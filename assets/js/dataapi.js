bfbc8d2bf48b26d2028ae8e67d0ccf499b171dbd
Kyosuke Kondo <kyosuke@bootscafe.jp>
2023-02-01 15:50:09 +0900

    さらに修正

--- assets/js/dataapi.js
+++ assets/js/dataapi.js
@@ -1,16 +1,16 @@
 $(document).ready(function(){
   $.getJSON("https://www.kurachic.jp/.data-api/v1/sites/84945/entries/?limit=5", function (results) {
-    console.log(results)
     $.each(results, function (i, item) {
       //var date_data = new Date(item.items[1].items[i].date);
       //var year = date_data.getFullYear();
       //var month = date_data.getMonth() + 1;
       //var day = date_data.getDate();
-      var title = item.items[1].items[i].title;
+      var title = item.items;
       //var link = item.items[1].items[i].permalink;
       //var date = year + '/' + month + '/' + day + '/';
       ///var category = item.items[1].items[i].categories[0];
       //$('#home-articles').append('<li><date>' + date + '</date><div class="category">' + category + '</div><div class="entry"><a href="' + link + '">' + title + '</a></div></li>');
+      console.log(title)
     });
   });
 });

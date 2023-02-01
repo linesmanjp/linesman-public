6b5da7cdd16abeeb57eae7bfe72e3d0d0b0af3d2
Kyosuke Kondo <kyosuke@bootscafe.jp>
2023-02-01 15:37:50 +0900

    混乱中

--- assets/js/dataapi.js
+++ assets/js/dataapi.js
@@ -1,15 +1,16 @@
 $(document).ready(function(){
   $.getJSON("https://www.kurachic.jp/.data-api/v1/sites/84945/entries/?limit=5", function (results) {
     $.each(results, function (i, item) {
-      var date_data = new Date(item.items[1].date);
-      var year = date_data.getFullYear();
-      var month = date_data.getMonth() + 1;
-      var day = date_data.getDate();
-      var title = item.items[1].title;
-      var link = item.items[1].permalink;
-      var date = year + '/' + month + '/' + day + '/';
-      var category = item.items[1].categories[0];
-      $('#home-articles').append('<li><date>' + date + '</date><div class="category">' + category + '</div><div class="entry"><a href="' + link + '">' + title + '</a></div></li>');
+      //var date_data = new Date(item.items[1].items[i].date);
+      //var year = date_data.getFullYear();
+      //var month = date_data.getMonth() + 1;
+      //var day = date_data.getDate();
+      var title = item.items[1].items[i].title;
+      //var link = item.items[1].items[i].permalink;
+      //var date = year + '/' + month + '/' + day + '/';
+      console.log(title)
+      ///var category = item.items[1].items[i].categories[0];
+      //$('#home-articles').append('<li><date>' + date + '</date><div class="category">' + category + '</div><div class="entry"><a href="' + link + '">' + title + '</a></div></li>');
     });
   });
 });

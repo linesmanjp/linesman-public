a278f312eea3fea6b84d3ec927b2fd2ccd8368c9
Kyosuke Koizumi (Kondo) <kyosuke@bootscafe.jp>
2023-02-01 14:57:28 +0900

    パース修正

--- assets/js/dataapi.js
+++ assets/js/dataapi.js
@@ -1,13 +1,13 @@
-$.getJSON("https://www.kurachic.jp/.data-api/v4/sites/84945/entries/", function (results) {
+$.getJSON("https://www.kurachic.jp/.data-api/v1/sites/84945/entries/?limit=5", function (results) {
   $.each(results, function (i, item) {
     var date_data = new Date(item.date);
     var year = date_data.getFullYear();
     var month = date_data.getMonth() + 1;
     var day = date_data.getDate();
-    var title = item.title.rendered;
-    var link = item.link;
+    var title = item.title;
+    var link = item.permalink;
     var date = year + '/' + month + '/' + day + '/';
-    var thum = item._embedded["wp:featuredmedia"][0].media_details.sizes.large.source_url
-    $('#home-articles .articles ul').append("<li><date>" + date + "</date><a href='" + link + "'><p></p><p>" + title + "</p></a></li>");
+    var category = item.entry.categories[0]
+    $('#home-articles .articles ul').append('<li><date>' + date + '</date><div class="category">'  + category +  '</div><div class="entry"><a href="' + link + '">' + title + '</a></div></li>");
   });
 });

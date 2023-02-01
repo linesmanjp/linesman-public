804a199bde2854db88fdd7eae751ce20112d1dd7
Kyosuke Kondo <kyosuke@bootscafe.jp>
2023-02-01 16:15:13 +0900

    修正

--- assets/js/dataapi.js
+++ assets/js/dataapi.js
@@ -5,7 +5,7 @@ $(document).ready(function(){
       //var year = date_data.getFullYear();
       //var month = date_data.getMonth() + 1;
       //var day = date_data.getDate();
-      var title = item.items;
+      var title = item[i].items;
       //var link = item.items[1].items[i].permalink;
       //var date = year + '/' + month + '/' + day + '/';
       ///var category = item.items[1].items[i].categories[0];

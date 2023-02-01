624429559b6e8e12b2814801eba025fd40bb0b7e
Kyosuke Koizumi (Kondo) <kyosuke@bootscafe.jp>
2023-02-01 14:45:54 +0900

    jQureyに変更

--- assets/js/dataapi.js
+++ assets/js/dataapi.js
@@ -1,11 +1,13 @@
-var request = new XMLHttpRequest();
- 
-request.open('GET', 'https://www.kurachic.jp/.data-api/v4/sites/84945/entries/', true);
-request.responseType = 'json';
-
-request.onload = function () {
-  var data = this.response;
-  console.log(data);
-};
-
-request.send();
+$.getJSON("https://www.kurachic.jp/.data-api/v4/sites/84945/entries/", function (results) {
+  $.each(results, function (i, item) {
+    var date_data = new Date(item.date);
+    var year = date_data.getFullYear();
+    var month = date_data.getMonth() + 1;
+    var day = date_data.getDate();
+    var title = item.title.rendered;
+    var link = item.link;
+    var date = year + '/' + month + '/' + day + '/';
+    var thum = item._embedded["wp:featuredmedia"][0].media_details.sizes.large.source_url
+    $('#home-articles .articles ul').append("<li><date>" + date + "</date><a href='" + link + "'><p></p><p>" + title + "</p></a></li>");
+  });
+});

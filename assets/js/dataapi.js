4ff5490f47c2d0e65d962ca7139131b78d3d6773
Kyosuke Koizumi (Kondo) <kyosuke@bootscafe.jp>
2023-02-02 10:57:09 +0900

    日付の書式

--- assets/js/dataapi.js
+++ assets/js/dataapi.js
@@ -5,27 +5,27 @@ $(document).ready(function(){
         var date_data0 = new Date(item[0].date);
         var year = date_data0.getFullYear();
         var month = ("0" + (date_data0.getMonth()+1)).slice(-2);
-        var day = ("0" + date_data0.getDate()).slice(-2)
+        var day = ("0" + date_data0.getDate()).slice(-2);
         var date0 = year + '/' + month + '/' + day;
         var date_data1 = new Date(item[1].date);
         var year = date_data1.getFullYear();
-        var month = date_data1.getMonth() + 1;
-        var day = date_data1.getDate();
+        var month = ("0" + (date_data1.getMonth()+1)).slice(-2);
+        var day = ("0" + date_data1.getDate()).slice(-2);
         var date1 = year + '/' + month + '/' + day;
         var date_data2 = new Date(item[2].date);
         var year = date_data2.getFullYear();
-        var month = date_data2.getMonth() + 1;
-        var day = date_data2.getDate();
+        var month = ("0" + (date_data2.getMonth()+1)).slice(-2);
+        var day = ("0" + date_data2.getDate()).slice(-2);
         var date2 = year + '/' + month + '/' + day;
         var date_data3 = new Date(item[3].date);
         var year = date_data3.getFullYear();
-        var month = date_data3.getMonth() + 1;
-        var day = date_data3.getDate();
+        var month = ("0" + (date_data3.getMonth()+1)).slice(-2);
+        var day = ("0" + date_data3.getDate()).slice(-2);
         var date3 = year + '/' + month + '/' + day;
         var date_data4 = new Date(item[4].date);
         var year = date_data4.getFullYear();
-        var month = date_data4.getMonth() + 1;
-        var day = date_data4.getDate();
+        var month = ("0" + (date_data4.getMonth()+1)).slice(-2);
+        var day = ("0" + date_data4.getDate()).slice(-2);
         var date4 = year + '/' + month + '/' + day;
         $('#home-articles').append('\
           <li><date>' + date0 + '</date><div class="category">' + item[0].categories[0].label + '</div><div class="entry"><a href="' + item[0].permalink + '" target="_blank">' + item[0].title + '</a></div></li>\
